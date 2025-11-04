fe3d0ce6987698c821a106398496fb7400add0cc
daffadhiya2301010024 <daffa061204@gmail.com>
2025-11-04 13:16:11 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
-        <p>&copy; 2024 Portfolio Website. Created by FAHMI SYARIEF HIDAYATULLAH & DAFFA DHIYA ULHAQ</p>
+        <p>&copy; 2024 Portfolio Website. Dibuat Oleh FAHMI SYARIEF HIDAYATULLAH & DAFFA DHIYA ULHAQ</p>
     </footer>
 </body>
+
 </html>
